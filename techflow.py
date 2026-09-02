39b831ab5bb0056280545843d2838c77d6c92205
Gustavo <inaciogustavo972@gmail.com>
2026-09-02 13:46:40 +0000

    Etapa do Mapeamento

--- techflow.py
+++ techflow.py
@@ -1,1 +1,21 @@
+#Ficha Cadastral de startup
 
+startups= {
+    "nome": "Flare solutions",
+    "fase": "Aceleração",
+    "ano_projeto": 2026
+}
+projeto_ativo = ["App Mobile","Automção IA"]
+print("STARTUP==",startups["nome"],"===")
+print("Projeto ativo em Sala:",projeto_ativo[0])
+
+#--Etapa 2, Mapeamento da Sala
+
+salas = [
+    ["Ocupada","livre"],
+    ["Ocupada","livre"]
+]
+print("Sala A1:",salas[0][0])
+print("Sala A2:",salas[0][1])
+print("Sala B1:",salas[1][0])
+print("Sala B2:",salas[1][1])
